7808ea89021eff5b9315c696209a4f600479a302
AAngel777 <l21211948@tectijuana.edu.mx>
2024-11-02 15:20:12 -0700

    Crud albino y home con graficas

--- machinemanagment/maquinaria/views.py
+++ machinemanagment/maquinaria/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import generic
 from .forms import MaquinariaForm
 from .models import Maquinaria
+from django.urls import reverse_lazy
 from django.db import transaction
 
 class MaquinariaView(generic.View):
@@ -24,3 +25,21 @@ class MaquinariaListView(generic.ListView):
     model = Maquinaria
     template_name = "maquinaria/maquinaria_list.html"
     context_object_name = "maquinarias"
+
+class MaquinariaDetailView(generic.DetailView):
+    model = Maquinaria
+    template_name = "maquinaria/maquinaria_detail.html"
+    context_object_name = "maquinaria"
+
+class MaquinariaUpdateView(generic.UpdateView):
+    model = Maquinaria
+    form_class = MaquinariaForm
+    template_name = "maquinaria/maquinaria_form.html"
+
+    def get_success_url(self):
+        return reverse_lazy('maquinaria:maquinaria_list')
+
+class MaquinariaDeleteView(generic.DeleteView):
+    model = Maquinaria
+    template_name = "maquinaria/maquinaria_confirm_delete.html"
+    success_url = reverse_lazy('maquinaria:maquinaria_list')
